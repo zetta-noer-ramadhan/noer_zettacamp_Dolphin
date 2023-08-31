c2e88747dc90e88e64c3ea18f628aba99b80cab3
zettacamp-noer-ramadhan <noer.ramadhan@zettacamp.pro>
2023-08-31 14:20:13 +0700

    [Update] On script
    - order
    - simple error handling on input

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 const state = {
     inventory: [],
-    is_edit: false,
-    is_add: false
+    is_edit: false
 }
 const storage = {
     create: (key, data) => localStorage.setItem(key, JSON.stringify(data)),
@@ -25,7 +24,7 @@ const BookModel = {
         storage.update('inventory', state.inventory)
     },
     readBooks: () => {
-        state.inventory = storage.read('inventory')
+        state.inventory = storage.read('inventory') || []
     },
     readBook: (id) => state.inventory.filter(book => book.id === id),
     updateBook: (id, title, author, description, count) => {
@@ -48,6 +47,160 @@ const BookModel = {
         storage.clear()
     }
 }
+
+
+const handleEdit = (id) => {
+    const bookRow = document.getElementById('book-' + id)
+    const input = bookRow.querySelectorAll('input')
+
+    storage.update('is_edit', true)
+
+    Array.from(input).forEach(item => {
+        item.disabled = !storage.read('is_edit')
+    })
+    const textarea = bookRow.querySelector('textarea')
+    textarea.disabled = !storage.read('is_edit')
+
+    bookRow.querySelector('.action--edit').className = "action--edit hide"
+    bookRow.querySelector('.action--save').className = "action--save"
+    bookRow.querySelector('.action--cancel').className = "action--cancel"
+    bookRow.querySelector('.action--delete').className = "action--delete hide"
+
+}
+const handleSave = (id) => {
+    const bookRow = document.getElementById('book-' + id)
+    const input = bookRow.querySelectorAll('input')
+    const textarea = bookRow.querySelector('textarea')
+
+    const [title, author, count] = Array.from(input).map(({ value }) => value)
+    const desc = textarea.value
+
+    if (!title) alert('please input book title')
+    if (!author) alert('please input book author')
+    if (!desc) alert('please input book description')
+    if (!count) alert('please input book count')
+    if (title && author && count && desc) {
+        storage.update('is_edit', false)
+
+        Array.from(input).forEach(item => {
+            item.disabled = !storage.read('is_edit')
+        })
+        textarea.disabled = !storage.read('is_edit')
+
+        bookRow.querySelector('.action--edit').className = "action--edit"
+        bookRow.querySelector('.action--save').className = "action--save hide"
+        bookRow.querySelector('.action--cancel').className = "action--cancel hide"
+        bookRow.querySelector('.action--delete').className = "action--delete"
+        BookModel.updateBook(id, title, author, desc, count)
+    }
+}
+const handleCancel = (id) => {
+    const bookRow = document.getElementById('book-' + id)
+    const input = bookRow.querySelectorAll('input')
+
+    storage.update('is_edit', false)
+
+    Array.from(input).forEach(item => {
+        item.disabled = !storage.read('is_edit')
+    })
+    const textarea = bookRow.querySelector('textarea')
+    textarea.disabled = !storage.read('is_edit')
+
+    bookRow.querySelector('.action--edit').className = "action--edit"
+    bookRow.querySelector('.action--save').className = "action--save hide"
+    bookRow.querySelector('.action--cancel').className = "action--cancel hide"
+    bookRow.querySelector('.action--delete').className = "action--delete"
+
+    RefreshRow(id)
+}
+const handleDelete = (id) => {
+    const bookRow = document.getElementById('book-' + id)
+    const input = bookRow.querySelectorAll('input')
+
+    const [title] = Array.from(input).filter(item => item.name == 'book-' + id + '-title').map(({ value }) => value)
+
+    const deleteConfirmation = confirm(`Are you sure to delete ${title}?`)
+    if (deleteConfirmation) {
+        BookModel.deleteBook(id)
+        RefreshTable()
+    }
+}
+const handleAdd = () => {
+    const element = document.querySelector('#book-add')
+    element.style.display = 'table-row'
+}
+const handleAddSave = () => {
+    const element = document.querySelector('#book-add')
+    const input = element.querySelectorAll('input')
+    const [title, author, count] = Array.from(input).map(({ value }) => value)
+    const textarea = element.querySelector('textarea')
+    const desc = textarea.value
+
+    if (!title) alert('please input book title')
+    if (!author) alert('please input book author')
+    if (!desc) alert('please input book description')
+    if (!count) alert('please input book count')
+    if (title && author && count && desc) {
+        BookModel.addBook(title, author, desc, count)
+        handleAddCancel()
+        RefreshTable()
+    }
+}
+const handleAddCancel = () => {
+    const element = document.querySelector('#book-add')
+    element.style.display = 'none'
+
+    const input = element.querySelectorAll('input')
+    Array.from(input).forEach(item => {
+        item.value = ""
+    })
+
+    const textarea = element.querySelector('textarea')
+    textarea.value = ""
+}
+const handleClear = () => {
+    const clearConfirmation = confirm(`Are you sure to clear the inventory?`)
+    if (clearConfirmation) {
+        storage.clear()
+        BookModel.readBooks()
+        RefreshTable()
+    }
+
+}
+
+
+const RefreshRow = (id) => {
+
+    const bookRow = document.getElementById('book-' + id)
+    const input = bookRow.querySelectorAll('input')
+
+    const currentBook = BookModel.readBook(id)[0]
+    // console.log(currentBook)
+
+    Array.from(input).forEach(item => {
+        console.log(id, item.name, item.value)
+        if (item.name == 'book-' + id + '-title') item.value = currentBook.title
+        if (item.name == 'book-' + id + '-author') item.value = currentBook.title
+        if (item.name == 'book-' + id + '-number') item.value = currentBook.count
+    })
+
+    const textarea = bookRow.querySelector('textarea')
+    textarea.value = currentBook.description
+}
+const RefreshTable = () => {
+    const table = document.getElementById('book__table__body')
+    Array.from(table.children).map((item) => {
+        item.remove()
+    })
+
+    if (state.inventory) {
+        Array.from(state.inventory).forEach(({ id, title, author, description, count }) => {
+            table.appendChild(PrepareTableItem(id, title, author, description, count))
+        })
+    }
+
+}
+
 
 const PrepareTableItem = (id, title, author, description, count) => {
     const row = document.createElement("tr")
@@ -87,148 +240,6 @@ const PrepareTableItem = (id, title, author, description, count) => {
 
     return row
 }
-const handleEdit = (id) => {
-    const bookRow = document.getElementById('book-' + id)
-    const input = bookRow.querySelectorAll('input')
-
-    storage.update('is_edit', true)
-
-    Array.from(input).forEach(item => {
-        item.disabled = !storage.read('is_edit')
-    })
-    const textarea = bookRow.querySelector('textarea')
-    textarea.disabled = !storage.read('is_edit')
-
-    bookRow.querySelector('.action--edit').className = "action--edit hide"
-    bookRow.querySelector('.action--save').className = "action--save"
-    bookRow.querySelector('.action--cancel').className = "action--cancel"
-    bookRow.querySelector('.action--delete').className = "action--delete hide"
-
-}
-const handleSave = (id) => {
-    const bookRow = document.getElementById('book-' + id)
-    const input = bookRow.querySelectorAll('input')
-
-    storage.update('is_edit', false)
-
-    Array.from(input).forEach(item => {
-        item.disabled = !storage.read('is_edit')
-    })
-    const textarea = bookRow.querySelector('textarea')
-    textarea.disabled = !storage.read('is_edit')
-
-    bookRow.querySelector('.action--edit').className = "action--edit"
-    bookRow.querySelector('.action--save').className = "action--save hide"
-    bookRow.querySelector('.action--cancel').className = "action--cancel hide"
-    bookRow.querySelector('.action--delete').className = "action--delete"
-
-    const [title, author, count] = Array.from(input).map(({ value }) => value)
-    const desc = textarea.value
-
-    BookModel.updateBook(id, title, author, desc, count)
-}
-const handleCancel = (id) => {
-    const bookRow = document.getElementById('book-' + id)
-    const input = bookRow.querySelectorAll('input')
-
-    storage.update('is_edit', false)
-
-    Array.from(input).forEach(item => {
-        item.disabled = !storage.read('is_edit')
-    })
-    const textarea = bookRow.querySelector('textarea')
-    textarea.disabled = !storage.read('is_edit')
-
-    bookRow.querySelector('.action--edit').className = "action--edit"
-    bookRow.querySelector('.action--save').className = "action--save hide"
-    bookRow.querySelector('.action--cancel').className = "action--cancel hide"
-    bookRow.querySelector('.action--delete').className = "action--delete"
-
-    RefreshRow(id)
-}
-const handleDelete = (id) => {
-    const bookRow = document.getElementById('book-' + id)
-    const input = bookRow.querySelectorAll('input')
-
-    const [title] = Array.from(input).filter(item => item.name == 'book-' + id + '-title').map(({ value }) => value)
-
-    const deleteConfirmation = confirm(`Are you sure to delete ${title}?`)
-    if(deleteConfirmation){
-        BookModel.deleteBook(id)
-        RefreshTable()
-    }
-}
-const handleAdd = () => {
-    const element = document.querySelector('#book-add')
-    element.style.display = 'table-row'
-}
-const handleAddSave = () => {
-    const element = document.querySelector('#book-add')
-    const input = element.querySelectorAll('input')
-    const [title, author, count] = Array.from(input).map(({ value }) => value)
-    const textarea = element.querySelector('textarea')
-    const desc = textarea.value
-    BookModel.addBook(title, author, desc, count)
-    handleAddCancel()
-    RefreshTable()
-}
-const handleAddCancel = () => {
-    const element = document.querySelector('#book-add')
-    element.style.display = 'none'
-
-    const input = element.querySelectorAll('input')
-    Array.from(input).forEach(item => {
-        item.value = ""
-    })
-
-    const textarea = element.querySelector('textarea')
-    textarea.value = ""
-}
-const handleClear = () => {
-    const clearConfirmation = confirm(`Are you sure to clear the inventory?`)
-    if (clearConfirmation){
-        storage.clear()
-        BookModel.readBooks()
-        RefreshTable()
-    }
-
-}
-
-
-const RefreshRow = (id) => {
-
-    const bookRow = document.getElementById('book-' + id)
-    const input = bookRow.querySelectorAll('input')
-
-    const currentBook = BookModel.readBook(id)[0]
-    // console.log(currentBook)
-
-    Array.from(input).forEach(item => {
-        console.log(id, item.name, item.value)
-        if (item.name == 'book-' + id + '-title') item.value = currentBook.title
-        if (item.name == 'book-' + id + '-author') item.value = currentBook.title
-        if (item.name == 'book-' + id + '-number') item.value = currentBook.count
-    })
-
-    const textarea = bookRow.querySelector('textarea')
-    textarea.value = currentBook.description
-}
-const RefreshTable = () => {
-    const table = document.getElementById('book__table__body')
-    Array.from(table.children).map((item) => {
-        item.remove()
-    })
-
-
-    if(state.inventory){
-        Array.from(state.inventory).forEach(({ id, title, author, description, count }) => {
-            table.appendChild(PrepareTableItem(id, title, author, description, count))
-        })
-    }
-
-
-}
-
 
 const main = () => {
 
