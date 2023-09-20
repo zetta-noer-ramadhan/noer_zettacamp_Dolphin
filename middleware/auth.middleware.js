b5ccb6fae99aa9cc08be4ad8a69160c5708b8fc0
zettacamp-noer-ramadhan <noer.ramadhan@zettacamp.pro>
2023-09-20 17:13:39 +0700

    [Init] elemMatch, arrayFilter, & distinct
    - add new functionality according to the task using elemMatch, arrayFilter, and distinct

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,43 @@
+
+const CheckAuthentication = (req, res, next) => {
+
+    const authorizaztionHeaders = req.headers["authorization"]
+
+    if (!authorizaztionHeaders) return res.status(401).json({
+        message: 'Access Denied',
+        detail: 'no auth header'
+    })
+
+    const [authType, usernameAndPassword] = authorizaztionHeaders.split(" ");
+
+    if (authType !== 'Basic') return res.status(401).json({
+        message: 'Access Denied',
+        detail: 'wrong auth type'
+    })
+
+    const [username, password] = Buffer.from(usernameAndPassword, "base64").toString("ascii").split(':')
+
+    if (!username || !password) return res.status(401).json({
+        message: 'Access Denied',
+        detail: 'no username or password'
+    })
+
+    const config = {
+        username: "uname",
+        password: "pword"
+    }
+
+    if (username !== config.username || password !== config.password) return res.status(401).json({
+        message: 'Access Denied',
+        detail: 'wrong username or password'
+    })
+
+    req.user = {
+        username,
+        password
+    }
+
+    return next()
+}
+
+module.exports = CheckAuthentication
