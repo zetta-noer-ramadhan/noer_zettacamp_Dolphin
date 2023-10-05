1a6c44030f7cbd818963b11a9dcb96abae022834
zettacamp-noer-ramadhan <noer.ramadhan@zettacamp.pro>
2023-10-05 11:36:03 +0700

    [Update] refactor on date time from object
    - using formatted object as an input for moment

--- app.js
+++ app.js
@@ -7,32 +7,57 @@ const ConvertMoment = (date, dateTime) => {
     const dateTimeFormat = dateFormat + ' ' + timeFormat
     const result = {}
 
+
+    const currentDateTime = moment()
+
     // #a current date & time
-    result.current_date = moment().format(dateTimeFormat)
+    result.current_date_time = currentDateTime.format(dateTimeFormat)
+
 
     // #d current date & time on utc
-    result.current_date_utc = moment().utc().format(dateTimeFormat)
+    result.current_date_time_utc = currentDateTime.utc().format(dateTimeFormat)
+
 
     // #e validation on string of date parameter
+    result.is_valid_string = false
     if (date && typeof date === 'string') {
-        result.is_valid_string = moment(date, dateFormat).isValid()
+        const dateTimeFromString = moment(date, dateFormat)
+        result.is_valid_string = dateTimeFromString.isValid()
 
         if (result.is_valid_string) {
             // #b date & time from string of date
-            result.datetime_from_string = moment(date, dateFormat).format(dateTimeFormat)
+            result.date_time_from_string = dateTimeFromString.format(dateTimeFormat)
         }
-    } else {
-        result.is_valid_string = false
     }
 
+
+    result.is_valid_object = false
     if (
         dateTime && typeof dateTime === 'object' &&
         dateTime.date && typeof dateTime.date === 'string' &&
         dateTime.time && typeof dateTime.time === 'string'
     ) {
-        // #c date & time from object of date and time
-        const formattedInput = dateTime.date + ' ' + dateTime.time
-        result.datetime_from_object = moment(formattedInput, dateTimeFormat).format(dateTimeFormat)
+        const dateSplit = dateTime.date.split('/').map(item => +item)
+        const [day, month, year] = dateSplit
+        const zeroBasedMonth = month > 0 ? month - 1 : month
+
+        const timeSplit = dateTime.time.split(':').map(item => +item)
+        const [hour, minute, second] = timeSplit
+
+        if (dateSplit.length === 3 && timeSplit.length === 3) {
+            // const formattedInput = dateTime.date + ' ' + dateTime.time
+            const formattedInput = { day, month: zeroBasedMonth, year, hour, minute, second }
+
+            // const dateTimeFromObject = moment(formattedInput, dateTimeFormat)
+            const dateTimeFromObject = moment(formattedInput)
+
+            result.is_valid_object = dateTimeFromObject.isValid()
+
+            if (result.is_valid_object) {
+                // #c date & time from object of date and time
+                result.date_time_from_object = dateTimeFromObject.format(dateTimeFormat)
+            }
+        }
     }
 
     console.log(result)
