5fa54dafe6b27ba49570da3ddfa8048719119a6f
zettacamp-noer-ramadhan <noer.ramadhan@zettacamp.pro>
2023-10-09 10:21:09 +0700

    [Update] refactor on validation

--- app.js
+++ app.js
@@ -6,24 +6,48 @@ const ErrorBuilder = (name, message) => ({
 });
 
 const DisplayAndQuery = (firstDate, secondDate) => {
-    if (!firstDate || typeof firstDate !== 'string' || !secondDate || typeof secondDate !== 'string') {
+    const isFirstDateExistAndAString = firstDate && typeof firstDate === 'string';
+    const isSecondDateExistAndAString = secondDate && typeof secondDate === 'string';
+
+    if (!isFirstDateExistAndAString || !isSecondDateExistAndAString) {
         return ErrorBuilder('parameter', 'bad strings of date parameter');
     }
 
-    if (!moment(firstDate, 'DD/MM/YYYY', true).isValid() || !moment(secondDate, 'DD/MM/YYYY', true).isValid()) {
-        return ErrorBuilder('invalid paramter', 'invalid strings of date parameter');
+    const isFirstDateValid = moment(firstDate, 'DD/MM/YYYY', true).isValid();
+    const isSecondDateValid = moment(secondDate, 'DD/MM/YYYY', true).isValid();
+
+    if (!isFirstDateValid || !isSecondDateValid) {
+        return ErrorBuilder('invalid parameter', 'invalid strings of date parameter');
+    }
+
+    const isSameOrAfterMoment = moment(firstDate, 'DD/MM/YYYY', true).isSameOrAfter(moment(secondDate, 'DD/MM/YYYY', true));
+    let isSameOrAfterMessage = '';
+
+    if (isSameOrAfterMoment) {
+        isSameOrAfterMessage = 'parameter 1\'s date is the same with or after parameter 2\'s';
+    } else {
+        isSameOrAfterMessage = 'parameter 1\'s date isn\'t the same with and not after parameter 2\'s';
+    }
+
+    const isBetweenMoment = moment().isBetween(moment(firstDate, 'DD/MM/YYYY', true), moment(secondDate, 'DD/MM/YYYY', true));
+    let isBetweenMessage = '';
+
+    if (isBetweenMoment) {
+        isBetweenMessage = 'the current date is between parameter 1\'s date and parameter 2\'s date';
+    } else {
+        isBetweenMessage = 'the current date isn\'t between parameter 1\'s date and parameter 2\'s date';
     }
 
     return {
-        first_date_indonesian: moment(firstDate, 'DD/MM/YYYY', true).locale('id').format('LL LTS'),
+        first_date_indonesian: moment(firstDate, 'DD/MM/YYYY', true).locale('id').format('dddd, LL LTS Z'),
         differences_in_week: moment(firstDate, 'DD/MM/YYYY', true).diff(moment(secondDate, 'DD/MM/YYYY', true), 'week'),
-        is_same_or_after: moment(firstDate, 'DD/MM/YYYY', true).isSameOrAfter(moment(secondDate, 'DD/MM/YYYY', true)),
-        is_between: moment().isBetween(moment(firstDate, 'DD/MM/YYYY', true), moment(secondDate, 'DD/MM/YYYY', true)),
+        is_same_or_after: isSameOrAfterMessage,
+        is_between: isBetweenMessage,
     };
 };
 
-const firstDate = '09/10/2023';
-const secondDate = '29/10/2023';
+const firstDate = '29/10/2023';
+const secondDate = '09/10/2023';
 
 const result = DisplayAndQuery(firstDate, secondDate);
 
